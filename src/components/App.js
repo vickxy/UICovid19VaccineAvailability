5451f2edcda83e59f163a4a03fc3f3f9d8018b30
aakanksha-02 <mail2aakanksha17@gmail.com>
2021-05-04 14:17:39 +0530

    heading changes

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ const  App = () => {
       <div className="heading">
         <h1>Vaccine Availability</h1>
         <h5>
-          Submit following details, and we'll notify you, as soon as vaccine
+          Submit following details, and we'll notify you, as soon as COVID-19 vaccine
           is available in your area for next 7 days.
         </h5>
       </div>
