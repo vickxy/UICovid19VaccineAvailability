bf960e1933ad536dfb9e4d591d63a9d77dcee42b
aakanksha-02 <mail2aakanksha17@gmail.com>
2021-05-04 22:22:47 +0530

    build

---
+++ build/precache-manifest.e412bc2170cf819f5b077eea3cfa9d39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c65e0a9d4e7a7ab71b1fc608e103baa2",
+    "url": "/vaccine-availability-covid19/index.html"
+  },
+  {
+    "revision": "ae4484d33a127d6a817d",
+    "url": "/vaccine-availability-covid19/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "c0274fe6740081195bf2",
+    "url": "/vaccine-availability-covid19/static/css/main.f15c9e5e.chunk.css"
+  },
+  {
+    "revision": "ae4484d33a127d6a817d",
+    "url": "/vaccine-availability-covid19/static/js/2.3b8c31a9.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/vaccine-availability-covid19/static/js/2.3b8c31a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0274fe6740081195bf2",
+    "url": "/vaccine-availability-covid19/static/js/main.59526e5a.chunk.js"
+  },
+  {
+    "revision": "3134f36ba381c04265fd",
+    "url": "/vaccine-availability-covid19/static/js/runtime-main.8380c7da.js"
+  }
+]);
